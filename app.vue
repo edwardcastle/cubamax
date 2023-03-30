510cf594ee428dace4a2905349e276f2a458845b
edwardcastle <eduardo.castillo@teavaro.com>
2023-03-30 17:24:16 -0400

    feat: Create second section.

--- app.vue
+++ app.vue
@@ -1,10 +1,10 @@
 <template>
   <!-- App -->
-    <div class="min-h-full">
-      <!-- Main content -->
-      <main class="">
-        <router-view />
-      </main>
-      <!-- End Main content -->
-    </div>
+  <div class="min-h-full">
+    <!-- Main content -->
+    <main class="">
+      <router-view />
+    </main>
+    <!-- End Main content -->
+  </div>
 </template>
---
+++ components/CubaWelcome.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="h-screen">
+    <img
+      class="object-cover w-full h-[80vh]"
+      src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+    />
+    <div class="relative grid grid-cols-12 mt-16">
+      <div
+        class="absolute col-span-10 col-start-2 sm:col-span-6 leading-[60px] sm:col-start-2 text-4xl sm:text-5xl bottom-0 h-[60vh] bg-accent opacity-70 p-12"
+      >
+        <p class="text-primary">
+          <span class="font-bold"> ¿Va a visitar a Cuba pronto?</span> Conozca
+          que debe tener en cuenta para su visita.
+        </p>
+        <div class="absolute bottom-10 w-full translate-x-1/3">
+          <button>
+            <img src="/static/icons/arrow-button.svg" alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
