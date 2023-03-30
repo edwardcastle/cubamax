4a9417ed5f0c916f14c5dd383c78805ccf708ca1
edwardcastle <eduardo.castillo@teavaro.com>
2023-03-30 17:22:55 -0400

    style: Fix image dimension problem.

--- components/AvaliableOptions.vue
+++ components/AvaliableOptions.vue
@@ -9,14 +9,13 @@ const props = defineProps({
 </script>
 
 <template>
-  <div v-if="icon" class="text-center mx-auto">
+  <div v-if="icon" class="text-center">
     <div class="card">
-
-  <img class="inset-0 object-cover my-auto mx-auto" :src="`/static/icons/${icon.img}`" />
-  <div class="mt-2 text-primary font-bold">
-    <p>{{ icon.name }}</p>
-  </div>
-</div>
+      <img class="inset-0 my-auto mx-auto" :src="`/static/icons/${icon.img}`" />
+      <div class="mt-2 text-primary font-bold">
+        <p>{{ icon.name }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
