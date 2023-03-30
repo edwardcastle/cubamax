e2335d52efd5c41d7d31ba85b48c3b7572757a2e
edwardcastle <eduardo.castillo@teavaro.com>
2023-03-30 17:23:30 -0400

    feat: Add right icons on topbar.

--- pages/index.vue
+++ pages/index.vue
@@ -47,23 +47,45 @@ const icons = reactive([
   <div class="min-h-screen">
     <!-- TopBar -->
     <div class="top-bar">
-      <!-- Logo -->
-      <div class="h-[97px] w-[292]">
-        <img src="static/icons/cubamax.svg" />
+      <div class="flex justify-between">
+        <!-- Logo -->
+        <div class="flex-auto my-auto">
+          <img src="static/icons/cubamax.svg" />
+        </div>
+        <!-- end Logo -->
+
+        <div class="hidden xs:block text-center text-lg text-primary">
+          <div class="columns-1 pl-10">
+            <p id="telf">(305)512-0330</p>
+          </div>
+          <div class="columns-1 mt-2">
+            <ul
+              class="p-2 text-center text-lg inline-flex align-baseline gap-x-4"
+            >
+              <li class="mt-2">
+                mas <br />
+                opciones
+              </li>
+              <li class="mt-3"><img src="static/icons/palm.svg" alt="" /></li>
+              <li class="mt-3"><img src="static/icons/user.svg" alt="" /></li>
+              <li><img src="static/icons/briefcase.svg" alt="" /></li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <!-- end Logo -->
-      <div></div>
     </div>
     <!-- end TopBar -->
 
     <!-- First Section-->
-    <div class="grid grid-cols-12 sm:px-12 mt-5">
-      <div class="flex flex-wrap gap-x-7 gap-y-14 col-span-12 lg:col-span-8 lg:col-start-3">
-      <avaliable-options v-for="icon in icons" :icon="icon" :key="icon.id" />
-    </div>
+    <div class="min-h-[90vh] py-10 grid grid-cols-12 mt-5">
+      <div
+        class="flex place-content-center align-middle flex-wrap gap-y-12 gap-x-10 col-span-12 md:col-span-10 md:col-start-2 lg:col-span-8 lg:col-start-3"
+      >
+        <avaliable-options v-for="icon in icons" :icon="icon" :key="icon.id" />
+      </div>
     </div>
     <!-- end First Section-->
-
+    <cuba-welcome />
     <!-- Second Section -->
 
     <!-- end Second Section-->
