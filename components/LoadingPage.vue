cb61ce8d24e320254d4b6dabd6efc41b75ea5391
edwardcastle <eduardo.castillo@teavaro.com>
2023-03-31 00:05:40 -0400

    feat: Create loading component

---
+++ components/LoadingPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex h-screen justify-center items-center">
+    <div class="h-64 w-6h-64">
+      <img src="static/icons/cubamax.svg" alt="" />
+    </div>
+  </div>
+</template>
